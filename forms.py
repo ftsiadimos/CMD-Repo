c1ddd33d815e1bef960a8401ea7f1f9e43d22cc4
Fotios Tsiadimos <ftsiadimos@gmail.com>
2026-02-06 15:39:39 -0500

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-class CommandForm(FlaskForm):
-    command = StringField("Shell Command", validators=[DataRequired()])
-    description = TextAreaField("Description (optional)")
-    tags = StringField("Tags (comma‑separated)")
-    submit = SubmitField("Save")
